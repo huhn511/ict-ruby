d1e75968843d1b64b3cc511f9680ea70134c393b
Unknown <sehe89@gmail.com>
2019-02-03 15:44:19 +0100

    fix request

--- lib/Ict.rb
+++ lib/Ict.rb
@@ -8,15 +8,28 @@ module Ict
     require 'uri'
     require 'net/http'
     require 'net/https'
+    require 'json'
 
     def getConfig
-
-      uri = URI.parse("http://localhost:8000/getConfig")
-      https = Net::HTTP.new(uri.host,uri.port)
-      req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
-      res = https.request(req)
-      pp "response"
-      pp res
+      send_request("getConfig")
     end
+
+    def send_request(endpoint)
+      url = URI.parse("http://localhost:2187/#{endpoint}")
+      data = {
+        password: 'testtest'
+      }
+      Net::HTTP.start(url.host, url.port) do |http|
+        req = Net::HTTP::Post.new(url.path)
+        req.set_form_data(data)
+        res = http.request(req)
+        if res && res.kind_of?(Net::HTTPSuccess)
+          return JSON.parse(res.body)
+        else
+          return "Something went wrong!"
+        end
+      end
+    end
+
   end
 end
